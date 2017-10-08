17beebecce8b0c0e8756c59b266a7579946d391b
Alexander Wilson <awilson@bellesetech.com>
2017-10-08 18:31:56 -0400

    style changes and ability changes

--- src/app/abilities/abilities.component.ts
+++ src/app/abilities/abilities.component.ts
@@ -17,13 +17,13 @@ export class AbilitiesComponent implements OnInit {
   starNumber: number;
 
   constructor() {
-    this.languages = [new Ability("Languages", "Java", 5), new Ability("Languages", "HTML", 4), new Ability("Languages", "CSS", 3), new Ability("Languages", "SQL", 3),
-      new Ability("Languages", "Typescript", 3), new Ability("Languages", "JQuery", 2), new Ability("Languages", "Python", 1), new Ability("Languages", "NodeJs", 1)];
-    this.frameworks = [new Ability("Frameworks", "AngularJs", 3)];
-    this.testTools = [new Ability("Test Tools", "Junit", 5), new Ability("Test Tools", "Fitnesse", 5), new Ability("Test Tools", "Postman", 3), new Ability("Test Tools", "Jasmine", 1)];
+    this.languages = [new Ability("Languages", "Java", 5), new Ability("Languages", "HTML", 5), new Ability("Languages", "CSS", 4),
+      new Ability("Languages", "Typescript", 4), new Ability("Languages", "SQL", 3), new Ability("Languages", "JQuery", 2), new Ability("Languages", "Python", 1), new Ability("Languages", "NodeJs", 1)];
+    this.frameworks = [new Ability("Frameworks", "Angular", 4)];
+    this.testTools = [new Ability("Test Tools", "Junit", 5), new Ability("Test Tools", "Fitnesse", 5), new Ability("Test Tools", "Postman", 3), new Ability("Test Tools", "Jasmine", 3)];
     this.databases = [new Ability("Databases", "Oracle", 4), new Ability("Databases", "H2", 2), new Ability("Databases", "Mongo", 1)];
     this.operatingSys = [new Ability("Operating Systems", "Windows", 5), new Ability("Operating Systems", "OSX", 5), new Ability("Operating Systems", "Linux", 3)];
-    this.devTools = [new Ability("Development Tools", "Eclipse", 5), new Ability("Development Tools","Netbeans", 4),  new Ability("Development Tools","GitLab", 4),
+    this.devTools = [new Ability("Development Tools", "Webstorm", 5), new Ability("Development Tools", "Eclipse", 5), new Ability("Development Tools","Netbeans", 4),  new Ability("Development Tools","GitLab", 4),
       new Ability("Development Tools","SourceTree", 3), new Ability("Development Tools","Maven", 3), new Ability("Development Tools","Latex", 3)];
     this.abilitySection = [this.languages, this.frameworks, this.testTools, this.databases, this.operatingSys, this.devTools]
 
