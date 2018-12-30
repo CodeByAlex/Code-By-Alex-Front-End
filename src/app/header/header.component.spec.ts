0cfee829b3fdb704ee3e10d3bd7635e2a25e92be
Wilson, Alexander <Alexander_Wilson@troweprice.com>
2018-12-29 21:50:18 -0500

    removed f from test

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -21,7 +21,7 @@ describe('HeaderComponent', () => {
     fixture.detectChanges();
   });
 
-  fit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
